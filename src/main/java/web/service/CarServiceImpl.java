801126711147b79f1777081c99d9cba00f90eac2
John Dee <zhuravleva_ta@ursip.ru>
2023-01-12 18:42:19 +0300

    перенос логики в service

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -5,14 +5,27 @@ import web.dao.CarDaoImpl;
 import web.model.Car;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService{
-    CarDao carDao = new CarDaoImpl();
+
+    private List<Car> carList;
+    {   carList = new ArrayList<>();
+
+        carList.add(new Car("Audi R8", "red", 2012));
+        carList.add(new Car("Audi TT", "grey", 2004));
+        carList.add(new Car("Carra GS7", "grey", 2018));
+        carList.add(new Car("Nissan", "white", 2006));
+        carList.add(new Car("RAM1500", "blue", 2012));
+    }
 
     @Override
     public List<Car> getCarList(Integer count) {
-        return carDao.getCarList(count);
+        if((count == null) || (count > carList.size())) {
+            count = carList.size();
+        }
+        return carList.subList(0, count);
     }
 }
